a864fb37083c4da17890763a8ae46f4756f09ab9
Artur Kabisov <kabisovartur6@gmail.com>
2025-12-11 01:37:15 +0300

    wrote first line imported all necessary

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 """Лабораторная работа номер 3 вариант 14"""
+
+import tkinter as tk
+from tkinter import ttk, messagebox, scrolledtext, simpledialog, filedialog
+import json
+import os
+import sys
+from datetime import datetime
